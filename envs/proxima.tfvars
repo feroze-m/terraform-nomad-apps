6d9122398a81bf4b9f47a837e5be9c3814785083
Feroze M <ferozem@Ferozes-MacBook-Air.local>
2022-04-22 11:10:30 -0400

    all servers added

--- envs/proxima.tfvars
+++ envs/proxima.tfvars
@@ -3,7 +3,6 @@ location_name = "nbg1"
 baseimage_name = "debian-10"
 
 bastion_count = 1
-#consulserver_count = 3
+consulserver_count = 3
 nomadserver_count = 3
-#nomadclient_count = 1
-
+nomadclient_count = 1
--- nomadserver.tf
+++ nomadserver.tf
@@ -1,9 +1,11 @@
-resource "hcloud_server" "bastion" {
-  name = format("nomadserver%2d", nomadserver_count)
+resource "hcloud_server" "nomadserver" {
+  name = format("nomadserver%02d", count.index + 1)
   count = var.nomadserver_count
   server_type = var.nomadserver_type
   image = var.baseimage_name
   location = var.location_name
+  firewall_ids = [hcloud_firewall.nomadserver_fw.id, hcloud_firewall.default.id]
+  placement_group_id = hcloud_placement_group.pg-1.id
 
   network {
     network_id = hcloud_network.network.id
@@ -17,6 +19,26 @@ resource "hcloud_server" "bastion" {
     hcloud_network_subnet.proxima_subnet
   ]
 }  
+
+resource "hcloud_firewall" "nomadserver_fw" {
+  name = "nomadserver-firewall"
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "4646"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_rdns" "nomadserver" {
+  count = var.nomadserver_count
+  server_id  = hcloud_server.nomadserver[count.index].id
+  ip_address = hcloud_server.nomadserver[count.index].ipv4_address
+  dns_ptr    = "${hcloud_server.nomadserver[count.index].name}.example.com"
+}
 
 variable "nomadserver_type" {
   type  = string
