4ad207c2da2ebdbf75ff0b2a4fd1a78ed3b49003
FerozeMohammad <ferozem@Ferozes-Mac-mini.local>
2022-05-10 18:00:26 -0400

    some changes

--- nomad_jobs/nomadjobs.tf
+++ nomad_jobs/nomadjobs.tf
@@ -1,8 +1,8 @@
 locals {}
 
 resource "nomad_job" "all_jobs" {
-    for_each = fileset(path.module, "apps/*")
-    jobspec = templatefile("${path.module}/${each.value}", {
+    for_each = fileset(path.module, "apps/*/*")
+    jobspec = templatefile("${path.module}/${each.value}.tmpl", {
         traefik_cpu = var.traefik_cpu
         traefik_mem = var.traefik_mem
     })
