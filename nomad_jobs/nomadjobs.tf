dad75922e7ee22efb89f52ff4ef52b7c5a8896d8
FerozeMohammad <ferozem@Ferozes-Mac-mini.local>
2022-05-10 23:35:56 -0400

    path changes

--- nomad_jobs/nomadjobs.tf
+++ nomad_jobs/nomadjobs.tf
@@ -1,8 +1,8 @@
 locals {}
 
 resource "nomad_job" "all_jobs" {
-    for_each = fileset(path.module, "apps/*")
-    jobspec = file("${path.module}/apps/${each.value}/jobspec.hcl")
+    for_each = fileset(path.module, "apps/*/*.hcl")
+    jobspec = file("${path.module}/${each.value}")
     deregister_on_destroy   = true
     purge_on_destroy        = true
 }
